aacfb8bf0f96bd895c291bf183dedb2550c57c20
huang-me <amos0107@gmail.com>
2020-10-17 16:48:31 +0800

    Delete repeat codes
    Calibrate when initializing the class, and save the variables needed.
    Calculate less or even just print in functions to save time.

--- project1_main.py
+++ project1_main.py
@@ -10,21 +10,27 @@ class MainWindow(QtWidgets.QMainWindow):
 		super(MainWindow, self).__init__()
 		self.ui = Ui_mainWindow()
 		self.ui.setupUi(self)
+		# connect buttons to functions
 		self.ui.BTN_CORNER.clicked.connect(self.corner_clicked)
 		self.ui.CLOSE_ALL_WINDOW.clicked.connect(self.close_win)
 		self.ui.BTN_INTRINSIC.clicked.connect(self.intrinsic)
 		self.ui.BTN_DISTORTION.clicked.connect(self.distort)
 		self.ui.ID_IMAGE.currentIndexChanged.connect(self.choosefile)
+		self.ui.BTN_EXTRINSIC.clicked.connect(self.extrinsic)
 		# initialize the combo box
 		self.ui.ID_IMAGE.addItems(['1', '2', '3', '4', '5', '6', \
 									'7', '8', '9', '10', '11', '12', \
 									'13', '14', '15'])
+		# class variables
 		self.extrinsic_num = 0
-		self.ui.BTN_EXTRINSIC.clicked.connect(self.extrinsic)
+		self.mtx = []
+		self.dist = []
+		self.tvec = []
+		self.rvecs = []
+		# initial calculate
+		self.calibrate()
 
-	def extrinsic(self):
-		# set window name
-		QtWidgets.QMainWindow.setWindowTitle(self, "1.3 Find Extrinsic")
+	def calibrate(self):
 		# camera calibration to compute K
 		criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 		w = 8
@@ -50,85 +56,33 @@ class MainWindow(QtWidgets.QMainWindow):
 		#calibration
 		ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 	
+		# variables to calculate Extrinsic matrix
 		objp = np.float32(objpoints).reshape(-1,3)
 		imgp = np.float32(imgpoints).reshape(-1,2)
-	
 		ret, rvec, tvec = cv.solvePnP(objp, imgp, mtx, dist)
 
-		R, _ = cv.Rodrigues(np.float32(rvecs[self.extrinsic_num - 1]))
+		# set the variable of the class
+		self.mtx = mtx
+		self.dist = dist
+		self.tvec = tvec
+		self.rvecs = rvecs
+
+	def extrinsic(self):
+		
+		R, _ = cv.Rodrigues(np.float32(self.rvecs[self.extrinsic_num - 1]))
 		# append R and T -> extrinsic matrix
-		ext = np.append(R, tvec, axis=1)
+		ext = np.append(R, self.tvec, axis=1)
 		print(ext)
-
-		QtWidgets.QMainWindow.setWindowTitle(self, "Main Window")
-
 	
 	def choosefile(self):
 		file_id = self.ui.ID_IMAGE.currentIndex() + 1
 		self.extrinsic_num = file_id
 
 	def distort(self):
-		# set window name
-		QtWidgets.QMainWindow.setWindowTitle(self, "1.4 Find Distortion")
-		# camera calibration to compute K
-		criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-		w = 8
-		h = 11
-		objp=np.zeros((w*h, 3), np.float32)
-		objp[:, :2]=np.mgrid[0:w, 0:h].T.reshape(-1, 2)
-		#store the world coord. and image coord. points
-		objpoints=[]
-		imgpoints=[]
-		images=glob.glob('Q1_Image/*.bmp')
-
-		for fname in images:
-			img=cv.imread(fname)
-			gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-			#find the corner of checkboard
-			ret, corners=cv.findChessboardCorners(gray, (w,h), None)
-			#save the points as long as find enough pair points
-			if ret == True:
-				cv.cornerSubPix(gray,corners,(8,11),(-1,-1),criteria)
-				objpoints.append(objp)
-				imgpoints.append(corners)
-
-		#calibration
-		ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-		print(dist)
-
-		QtWidgets.QMainWindow.setWindowTitle(self, "Main Window")
-
+		print(self.dist)
 
 	def intrinsic(self):
-		# set window name
-		QtWidgets.QMainWindow.setWindowTitle(self, "1.2 Find Intrinsic")
-		# camera calibration to compute K
-		criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-		w = 8
-		h = 11
-		objp=np.zeros((w*h, 3), np.float32)
-		objp[:, :2]=np.mgrid[0:w, 0:h].T.reshape(-1, 2)
-		#store the world coord. and image coord. points
-		objpoints=[]
-		imgpoints=[]
-		images=glob.glob('Q1_Image/*.bmp')
-
-		for fname in images:
-			img=cv.imread(fname)
-			gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-			#find the corner of checkboard
-			ret, corners=cv.findChessboardCorners(gray, (w,h), None)
-			#save the points as long as find enough pair points
-			if ret == True:
-				cv.cornerSubPix(gray,corners,(8,11),(-1,-1),criteria)
-				objpoints.append(objp)
-				imgpoints.append(corners)
-
-		#calibration
-		ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-		print(mtx)
-		QtWidgets.QMainWindow.setWindowTitle(self, "Main Window")
-
+		print(self.mtx)
 
 	def corner_clicked(self):
 		# set window name
